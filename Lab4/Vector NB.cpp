fcf3bd1949cfea1eb8d1f6d63d758debc58e14a8
OlgaKovalchuk <65169012+OlgaKovalchuk@users.noreply.github.com>
2021-01-11 18:55:59 +0200

    Update Vector NB.cpp

--- Lab4/Vector NB.cpp
+++ Lab4/Vector NB.cpp
@@ -13,19 +13,19 @@ int main()
 	string vector2;
 	string generator;
 	int expansion;
-	cout << "Введите вектор 1:" << endl;
+	cout << "Р’РІРµРґРёС‚Рµ РІРµРєС‚РѕСЂ 1:" << endl;
 	cin >> vector1;
 	//vector1 = revers_big_string(vec1, vec1.length());
-	cout << "Введите вектор 2:" << endl;
+	cout << "Р’РІРµРґРёС‚Рµ РІРµРєС‚РѕСЂ 2:" << endl;
 	cin >> vector2;
 	//vector2 = revers_big_string(vec2, vec2.length());
-	/*cout << "Введите генератор:" << endl;
+	/*cout << "Р’РІРµРґРёС‚Рµ РіРµРЅРµСЂР°С‚РѕСЂ:" << endl;
 	cin >> gener;
 	generator = revers_big_string(gener, gener.length());*/
 	expansion = vector1.length();
-	cout << endl << "Степень расширения поля: " << expansion << endl;
-	cout << "Конвертируем строки в масивы интов:" << endl;
-	//Конвертируем вектор 1
+	cout << endl << "РЎС‚РµРїРµРЅСЊ СЂР°СЃС€РёСЂРµРЅРёСЏ РїРѕР»СЏ: " << expansion << endl;
+	cout << "РљРѕРЅРІРµСЂС‚РёСЂСѓРµРј СЃС‚СЂРѕРєРё РІ РјР°СЃРёРІС‹ РёРЅС‚РѕРІ:" << endl;
+	//РљРѕРЅРІРµСЂС‚РёСЂСѓРµРј РІРµРєС‚РѕСЂ 1
 	int* _vector_1 = new int[expansion - 1];
 	element_null(_vector_1, expansion - 1);
 	convert_str_to_bin(vector1, _vector_1);
@@ -39,9 +39,9 @@ int main()
 	{
 		vector11 = vector11 + bin_to_string(_vector_1[i]);
 	}
-	cout << "Вектор 1:" << endl;
+	cout << "Р’РµРєС‚РѕСЂ 1:" << endl;
 	cout << vector11 << endl;
-	//Конвертируем вектор 2
+	//РљРѕРЅРІРµСЂС‚РёСЂСѓРµРј РІРµРєС‚РѕСЂ 2
 	int* _vector_2 = new int[expansion - 1];
 	element_null(_vector_2, expansion - 1);
 	convert_str_to_bin(vector2, _vector_2);
@@ -50,25 +50,25 @@ int main()
 		cout << _vector_1[i] << " ";
 	}
 	cout << endl;
-	//Конвертируем генератор
+	//РљРѕРЅРІРµСЂС‚РёСЂСѓРµРј РіРµРЅРµСЂР°С‚РѕСЂ
 	/*int* _generator = new int[expansion];
 	element_null(_generator, expansion);
 	convert_str_to_bin(generator, _generator);
-	cout << endl << "Первый вектор (reversed):" << endl;
+	cout << endl << "РџРµСЂРІС‹Р№ РІРµРєС‚РѕСЂ (reversed):" << endl;
 	for (int i = 0; i < expansion; i++)
 	{
 		cout << _vector_1[i] << " ";
 	}
 	cout << endl;
 	//cout << revers_output(_vector_1, expansion - 1);
-	cout << endl << "Второй ветор (reversed):" << endl;
+	cout << endl << "Р’С‚РѕСЂРѕР№ РІРµС‚РѕСЂ (reversed):" << endl;
 	for (int i = 0; i < expansion; i++)
 	{
 		cout << _vector_2[i] << " ";
 	}
 	cout << endl;
 	//cout << revers_output(_vector_2, expansion - 1);
-	cout << endl << "Генератор (reversed):" << endl;
+	cout << endl << "Р“РµРЅРµСЂР°С‚РѕСЂ (reversed):" << endl;
 	for (int i = 0; i < expansion; i++)
 	{
 		cout << _generator[i] << " ";
@@ -77,20 +77,20 @@ int main()
 	//cout << revers_output(_generator, expansion);*/
 
 	cout << endl;
-	cout << "Какую операцию Вы хотите выполнить:" << endl;
+	cout << "РљР°РєСѓСЋ РѕРїРµСЂР°С†РёСЋ Р’С‹ С…РѕС‚РёС‚Рµ РІС‹РїРѕР»РЅРёС‚СЊ:" << endl;
 	cout << endl;
-	cout << "[0]   Константный 0;" << endl;
-	cout << "[1]   Константная 1;" << endl;
-	cout << "[+]   Сумма;" << endl;
-	cout << "[*]   Умножение;" << endl;
-	cout << "[^]   Возведение в степень;" << endl;
-	cout << "[s]   Возведение в квадрат;" << endl;
-	cout << "[i]   Обратный элемент;" << endl;
-	cout << "[t]   След;" << endl;
-	cout << "Введите свой выбор:" << endl;
+	cout << "[0]   РљРѕРЅСЃС‚Р°РЅС‚РЅС‹Р№ 0;" << endl;
+	cout << "[1]   РљРѕРЅСЃС‚Р°РЅС‚РЅР°СЏ 1;" << endl;
+	cout << "[+]   РЎСѓРјРјР°;" << endl;
+	cout << "[*]   РЈРјРЅРѕР¶РµРЅРёРµ;" << endl;
+	cout << "[^]   Р’РѕР·РІРµРґРµРЅРёРµ РІ СЃС‚РµРїРµРЅСЊ;" << endl;
+	cout << "[s]   Р’РѕР·РІРµРґРµРЅРёРµ РІ РєРІР°РґСЂР°С‚;" << endl;
+	cout << "[i]   РћР±СЂР°С‚РЅС‹Р№ СЌР»РµРјРµРЅС‚;" << endl;
+	cout << "[t]   РЎР»РµРґ;" << endl;
+	cout << "Р’РІРµРґРёС‚Рµ СЃРІРѕР№ РІС‹Р±РѕСЂ:" << endl;
 	char answer;
 	cin >> answer;
-	cout << "Ваш результат:" << endl;
+	cout << "Р’Р°С€ СЂРµР·СѓР»СЊС‚Р°С‚:" << endl;
 
 	switch (answer)
 	{
@@ -104,7 +104,7 @@ int main()
 		cout << revers_output(element_null(result, expansion), expansion) << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы выведения нейтрального элемента по суммированию : " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РІС‹РІРµРґРµРЅРёСЏ РЅРµР№С‚СЂР°Р»СЊРЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р° РїРѕ СЃСѓРјРјРёСЂРѕРІР°РЅРёСЋ : " << d1 << endl;
 		//delete[] result;
 	}
 	break;
@@ -119,7 +119,7 @@ int main()
 		cout << revers_output(element_one(result, expansion), expansion) << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы выведения нейтрального элемента  по умножению -- 1: " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РІС‹РІРµРґРµРЅРёСЏ РЅРµР№С‚СЂР°Р»СЊРЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р°  РїРѕ СѓРјРЅРѕР¶РµРЅРёСЋ -- 1: " << d1 << endl;
 		//delete[] result;
 	}
 	break;
@@ -140,7 +140,7 @@ int main()
 		cout << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы операции + : " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РѕРїРµСЂР°С†РёРё + : " << d1 << endl;
 		//delete[] result;
 	}
 	break;
@@ -156,7 +156,7 @@ int main()
 		cout << revers_output(multiplicative_vector(_vector_1, _vector_2, result, expansion), expansion) << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы операции * : " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РѕРїРµСЂР°С†РёРё * : " << d1 << endl;
 		//delete[] result;
 	}
 	break;
@@ -168,18 +168,18 @@ int main()
 		srand(time(NULL));
 		start1 = clock();
 		string deg;
-		cout << "Введите степень в бинарном виде:" << endl;
+		cout << "Р’РІРµРґРёС‚Рµ СЃС‚РµРїРµРЅСЊ РІ Р±РёРЅР°СЂРЅРѕРј РІРёРґРµ:" << endl;
 		cin >> deg;
 
 		string degree = revers_big_string(deg, deg.length());
 		int size_degree = degree.length();
-		//Конвертируем степень в масив интов
+		//РљРѕРЅРІРµСЂС‚РёСЂСѓРµРј СЃС‚РµРїРµРЅСЊ РІ РјР°СЃРёРІ РёРЅС‚РѕРІ
 		int* _degree = new int[size_degree];
 		element_null(_degree, size_degree);
 		convert_str_to_bin(degree, _degree);
 
 
-		cout << endl << "Возведение первого вектора в степень:" << endl;
+		cout << endl << "Р’РѕР·РІРµРґРµРЅРёРµ РїРµСЂРІРѕРіРѕ РІРµРєС‚РѕСЂР° РІ СЃС‚РµРїРµРЅСЊ:" << endl;
 
 		int* result = new int[expansion - 1];
 		element_null(result, expansion);
@@ -187,14 +187,14 @@ int main()
 		//delete[] result;
 
 
-		cout << endl << "Возведение второго вектора в степень:" << endl;
+		cout << endl << "Р’РѕР·РІРµРґРµРЅРёРµ РІС‚РѕСЂРѕРіРѕ РІРµРєС‚РѕСЂР° РІ СЃС‚РµРїРµРЅСЊ:" << endl;
 
 		int* result_ = new int[expansion - 1];
 		element_null(result_, expansion);
 		//cout << revers_output(elevation_to_huge_degree(_vector_2, _degree, _generator, result_, expansion), expansion) << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы операции возведения в большую степень для двух векторов : " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РѕРїРµСЂР°С†РёРё РІРѕР·РІРµРґРµРЅРёСЏ РІ Р±РѕР»СЊС€СѓСЋ СЃС‚РµРїРµРЅСЊ РґР»СЏ РґРІСѓС… РІРµРєС‚РѕСЂРѕРІ : " << d1 << endl;
 		//delete[] result_;
 		//delete[] _degree;
 	}
@@ -206,7 +206,7 @@ int main()
 		clock_t start1, finish1;
 		srand(time(NULL));
 		start1 = clock();
-		cout << endl << "Первый вектор в квадрате:" << endl;
+		cout << endl << "РџРµСЂРІС‹Р№ РІРµРєС‚РѕСЂ РІ РєРІР°РґСЂР°С‚Рµ:" << endl;
 
 		int* result = new int[expansion - 1];
 		element_null(result, expansion);
@@ -218,7 +218,7 @@ int main()
 		cout << endl;
 		//delete[] result;
 
-		cout << endl << "Второй вектор в квадрате:" << endl;
+		cout << endl << "Р’С‚РѕСЂРѕР№ РІРµРєС‚РѕСЂ РІ РєРІР°РґСЂР°С‚Рµ:" << endl;
 
 		int* result_ = new int[expansion - 1];
 		element_null(result_, expansion);
@@ -230,7 +230,7 @@ int main()
 		cout << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы операции ^ для двух векторов : " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РѕРїРµСЂР°С†РёРё ^ РґР»СЏ РґРІСѓС… РІРµРєС‚РѕСЂРѕРІ : " << d1 << endl;
 		//delete[] result_;
 	}
 	break;
@@ -256,13 +256,13 @@ int main()
 				number_of_one2++;
 			}
 		}
-		cout << "Количество единиц в векторе 1: " << number_of_one1 << endl;
-		cout << "Количество единиц в векторе 2: " << number_of_one2 << endl;
-		cout << endl << "След первого вектора:" << trace_normal(_vector_1, expansion ) << endl;
-		cout << endl << "След второго вектора:" << trace_normal(_vector_2, expansion ) << endl;
+		cout << "РљРѕР»РёС‡РµСЃС‚РІРѕ РµРґРёРЅРёС† РІ РІРµРєС‚РѕСЂРµ 1: " << number_of_one1 << endl;
+		cout << "РљРѕР»РёС‡РµСЃС‚РІРѕ РµРґРёРЅРёС† РІ РІРµРєС‚РѕСЂРµ 2: " << number_of_one2 << endl;
+		cout << endl << "РЎР»РµРґ РїРµСЂРІРѕРіРѕ РІРµРєС‚РѕСЂР°:" << trace_normal(_vector_1, expansion ) << endl;
+		cout << endl << "РЎР»РµРґ РІС‚РѕСЂРѕРіРѕ РІРµРєС‚РѕСЂР°:" << trace_normal(_vector_2, expansion ) << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы операции вычисления следа элемента : " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РѕРїРµСЂР°С†РёРё РІС‹С‡РёСЃР»РµРЅРёСЏ СЃР»РµРґР° СЌР»РµРјРµРЅС‚Р° : " << d1 << endl;
 	}
 	break;
 
@@ -272,33 +272,33 @@ int main()
 		clock_t start1, finish1;
 		srand(time(NULL));
 		start1 = clock();
-		cout << endl << "Обратный к первому вектор:" << endl;
+		cout << endl << "РћР±СЂР°С‚РЅС‹Р№ Рє РїРµСЂРІРѕРјСѓ РІРµРєС‚РѕСЂ:" << endl;
 		int* result = new int[expansion - 1];
 		element_null(result, expansion);
 		//cout << revers_output(inverse_vector(_vector_1, _generator, result, expansion), expansion) << endl;
 		//delete[] result;
 
-		cout << endl << "Обратный ко второму вектор:" << endl;
+		cout << endl << "РћР±СЂР°С‚РЅС‹Р№ РєРѕ РІС‚РѕСЂРѕРјСѓ РІРµРєС‚РѕСЂ:" << endl;
 		int* result_ = new int[expansion - 1];
 		element_null(result_, expansion);
 		//cout << revers_output(inverse_vector(_vector_2, _generator, result_, expansion), expansion) << endl;
 		finish1 = clock();
 		d1 = (double)(finish1 - start1) / CLOCKS_PER_SEC;
-		cout << "Время работы операции поиска обратного элемента: " << d1 << endl;
+		cout << "Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РѕРїРµСЂР°С†РёРё РїРѕРёСЃРєР° РѕР±СЂР°С‚РЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р°: " << d1 << endl;
 		//delete[] result_;
 	}
 	break;
 
 	default:
-		cout << "Такого символа нет среди вышеуказаных!" << endl;
+		cout << "РўР°РєРѕРіРѕ СЃРёРјРІРѕР»Р° РЅРµС‚ СЃСЂРµРґРё РІС‹С€РµСѓРєР°Р·Р°РЅС‹С…!" << endl;
 		break;
 
 	}
 	cout << "=======================================================================" << endl;
-	cout << "Проверка (a+b)+c=a+(b+c)" << endl;
+	cout << "РџСЂРѕРІРµСЂРєР° (a+b)+c=a+(b+c)" << endl;
 	string vec3;
 	string vector3;
-	cout << "Введите вектор 3:" << endl;
+	cout << "Р’РІРµРґРёС‚Рµ РІРµРєС‚РѕСЂ 3:" << endl;
 	cin >> vec3;
 	vector3 = revers_big_string(vec3, vec3.length());
 	int* _vector_3 = new int[expansion - 1];
@@ -306,49 +306,49 @@ int main()
 	convert_str_to_bin(vector3, _vector_3);
 	int* resultAdd1 = new int[expansion - 1];
 	element_null(resultAdd1, expansion);
-	cout << "Результат (a+b):" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ (a+b):" << endl;
 	cout << revers_output(add_vector(_vector_1, _vector_2, resultAdd1, expansion), expansion) << endl;
 	int* resultAdd2 = new int[expansion - 1];
 	element_null(resultAdd2, expansion);
-	cout << "Результат (a+b)+c:" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ (a+b)+c:" << endl;
 	cout << revers_output(add_vector(resultAdd1, _vector_3, resultAdd2, expansion), expansion) << endl;
-	cout << "Считаем a+(b+c):" << endl;
+	cout << "РЎС‡РёС‚Р°РµРј a+(b+c):" << endl;
 	int* resultAdd3 = new int[expansion - 1];
 	element_null(resultAdd3, expansion);
-	cout << "Результат (b+c):" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ (b+c):" << endl;
 	cout << revers_output(add_vector(_vector_2, _vector_3, resultAdd3, expansion), expansion) << endl;
 	int* resultAdd4 = new int[expansion - 1];
 	element_null(resultAdd4, expansion);
-	cout << "Результат a+(b+c):" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ a+(b+c):" << endl;
 	cout << revers_output(add_vector(resultAdd3, _vector_1, resultAdd4, expansion), expansion) << endl;
 	//delete[]resultAdd1;
 	//delete[]resultAdd2;
 	//delete[]resultAdd3;
 	//delete[]resultAdd4;
-	cout << "Проверка a+b+0=0+a+b=a+b";
-	cout << "Считаем a+b+0:" << endl;
+	cout << "РџСЂРѕРІРµСЂРєР° a+b+0=0+a+b=a+b";
+	cout << "РЎС‡РёС‚Р°РµРј a+b+0:" << endl;
 	int* resultAdd5 = new int[expansion - 1];
 	element_null(resultAdd5, expansion);
-	cout << "Результат a+b:" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ a+b:" << endl;
 	cout << revers_output(add_vector(_vector_1, _vector_2, resultAdd5, expansion), expansion) << endl;
 	int* resultAdd6 = new int[expansion - 1];
 	element_null(resultAdd6, expansion);
 	int* null = new int[expansion - 1];
 	element_null(null, expansion);
-	cout << "Результат a+b+0:" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ a+b+0:" << endl;
 	cout << revers_output(add_vector(resultAdd5, null, resultAdd6, expansion), expansion) << endl;
-	cout << "Считаем 0+a+b:" << endl;
+	cout << "РЎС‡РёС‚Р°РµРј 0+a+b:" << endl;
 	int* resultAdd7 = new int[expansion - 1];
 	element_null(resultAdd7, expansion);
-	cout << "Результат 0+a:" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ 0+a:" << endl;
 	cout << revers_output(add_vector(null, _vector_1, resultAdd7, expansion), expansion) << endl;
 	int* resultAdd8 = new int[expansion - 1];
 	element_null(resultAdd8, expansion);
-	cout << "Результат 0+a+b:" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ 0+a+b:" << endl;
 	cout << revers_output(add_vector(resultAdd7, _vector_2, resultAdd8, expansion), expansion) << endl;
 	int* resultAdd9 = new int[expansion - 1];
 	element_null(resultAdd9, expansion);
-	cout << "Результат a+b:" << endl;
+	cout << "Р РµР·СѓР»СЊС‚Р°С‚ a+b:" << endl;
 	cout << revers_output(add_vector(_vector_1, _vector_2, resultAdd9, expansion), expansion) << endl;
 	//delete[]resultAdd5;
 	//delete[]resultAdd6;
